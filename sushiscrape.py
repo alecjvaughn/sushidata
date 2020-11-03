90a149607ecf3f2055ddeab21732b9d0bf514908
Alec Vaughn <alecj.vaughn@gmail.com>
2020-11-03 00:00:42 -0600

    Add local path retries exeeded

--- sushiscrape.py
+++ sushiscrape.py
@@ -4,8 +4,10 @@ import time
 from bs4 import BeautifulSoup
 
 url = 'https://www.thrillist.com/eat/nation/fattest-sushi-rolls-sushi-rolls-by-calorie'
+path = "/Users/alecjvaughn/Developer/py/sushi/fattest-sushi-rolls-sushi-rolls-by-calorie.json"
 
-response = requests.get(url)
+# response = requests.get(url)
+response = requests.get(path)
 print(response)
 
 soup = BeautifulSoup(response.text, "html.parser")
@@ -14,6 +16,8 @@ divs = soup.find_all('div', class_='paragraph')
 for div in divs:
     roll = div.find('h2')
     ingredients = div.find('em')
+    # print(type(roll))
+    # print(type(ingredients))
     if None in (roll, ingredients):
         continue
     print(roll)
