92e1fbc9010f7897769399c0cc9db653ede39d11
Alec Vaughn <alecj.vaughn@gmail.com>
2020-11-04 22:01:42 -0600

    Update udf to class and print to file

--- sushiscrape.py
+++ sushiscrape.py
@@ -1,13 +1,16 @@
-from bs4.element import SoupStrainer
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
+import sys
 
 
-def sushiRoll(name, ingredients):
-    name
-    ingredients
+class sushiRoll:
+
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
+
+    def string(self):
+        return f"{self.name}: {self.ingredients}"
 
 url = 'https://www.thrillist.com/eat/nation/fattest-sushi-rolls-sushi-rolls-by-calorie'
 copy = open("/Users/alecjvaughn/Developer/py/sushi/fattest-sushi-rolls-sushi-rolls-by-calorie.html")
@@ -18,15 +21,22 @@ copy = open("/Users/alecjvaughn/Developer/py/sushi/fattest-sushi-rolls-sushi-rol
 soup = BeautifulSoup(copy, "html.parser")
 
 divs = soup.find_all('div', class_='paragraph')
-sushiTable = {} # <ingredient, rolls>
+sushiTable = {}
 for div in divs:
     roll = div.find('h2')
     ingredients = div.find('em')
     if None in (roll, ingredients):
         continue
-    mySushi = sushiRoll(roll, ingredients)
-    for ingredient in ingredients:
-        sushiTable[ingredient] = list.append(mySushi)
-print(sushiTable)
+    mySushi = sushiRoll(roll.text, ingredients.text.split(","))
+    for ingredient in ingredients.text.split(","):
+        if ingredient not in sushiTable:
+            sushiList = [sushiRoll.string]
+            sushiTable[ingredient] = sushiList
+        else:
+            sushiTable[ingredient].append(sushiRoll.string)
+myFile = open('mySushiData.txt', 'w')
+for key in sushiTable:
+    myFile.write(f"{key}: ${sushiTable[key]}\n")
+print(sys.exit)
 
 
